614f5da79176a4d239aafb75db2bc5e344197598
Bondar Ferenc <fhery021@gmail.com>
2019-01-19 00:25:42 +0200

    Specialties service map implementation created. closes #31

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/SpecialtiesService.java
@@ -0,0 +1,9 @@
+package guru.springframework.sfgpetclinic.services;
+
+import guru.springframework.sfgpetclinic.model.Speciality;
+
+/**
+ * Created by Ferenc on 1/19/2019.
+ */
+public interface SpecialtiesService extends CrudService<Speciality, Long>  {
+}
